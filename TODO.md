f66cd8eb56752144169e17a796d09989be0e119b
katherine <kes.sullivan1@gmail.com>
2022-11-09 15:39:56 -0300

    algo de avance en closureConvert

--- TODO.md
+++ TODO.md
@@ -17,3 +17,19 @@
  - Implementar un esquema de compilación para el operador print usando las instrucciones PRINT y PRINTN.
  - (Opcional) Reduzca el tamaño del bytecode generado usando un solo byte para la codificación de las instrucciones. Adapte la máquina para funcionar con su nuevo formato. Puede también mejorar la codificación de las cadenas usando UTF-8 (en vez de UTF-32).
  - (Opcional) Compare la performance de computar ack 3 11 en la Macchina con la de un programa C nativo. Explique la discrepancia.
+
+ --
+
+## Clausure conversion
+
+Pasada sobre el programa para convertir cada funcion en una creacion de clausura. Para ello, usamos la sintaxis (fun (x:τ ) -> ...)[v1, . . . , vn] para indicar la creacion de una clausura para la funcion dada. Las variables libres de la funcion son reemplazadas por variables distinguidas de la forma ei, las cuales toman los valores vi
+
+llamado rec -> ver lo que es
+
+en padre de todo -> tengo una sola var libre -> agarro lo del llamado rec si es una clausura 
+
+variables libres -> 
+
+## Hoisting
+
+Recorremos todo el termino, reemplazando cada fun por un nombre, y generando una definicion top-level de ese nombre con el mismo cuerpo (tomar el resultado de la conversi ́on de clausuras y convertir las funciones lambda dentro de las creaciones de clausuras en definiciones globales, cada una con algun nombre fresco)
--- src/ClosureConvert.hs
+++ src/ClosureConvert.hs
@@ -4,6 +4,7 @@ import Lang
 import IR
 import Control.Monad.State
 import Control.Monad.Writer
+import Parse (tm)
 
 type M a = StateT Int (Writer [IrDecl]) a
 
@@ -13,20 +14,32 @@ closureConvert (V _ (Bound i)) = return
 closureConvert (V _ (Free n)) = return $ IrVar n
 closureConvert (V _ (Global n)) = return $ IrGlobal n
 closureConvert (Const _ c) = return $ IrConst c
-closureConvert (Lam (_,fty) n _ s) = do fr <-  get
-                                        put $ fr + 1
-                                        frName <- "__f" ++ show fr
-
-                                        ird <- IrFun frName (convertType fty) 
+closureConvert (Lam (_,fty) n _ (Sc1 t)) = do  fr <-  get
+                                               put $ fr + 1
+                                               frName <- "__f" ++ show fr
+                                               body <- closureConvert t
+                                               ird <- IrFun frName (convertType fty) args body
+                                               tell ird
+                                               return $ MkClosure frName args
+                                        -- arnar mk clousure
+-- 1. armar el irdecl y gurdarlo en la monada
+-- 2. devolver el MkClosure
+--  name -> fresco
+--  args ?
+--  body -> llamada rec + cerrar y subst 1 vez ?
 
 closureConvert (BinaryOp _ op t1 t2) = return $ IrBinaryOp op (closureConvert t1) (closureConvert t2)
 closureConvert (IfZ _ t1 t2 t3) = return $ IrIfZ (closureConvert t1) (closureConvert t2) (closureConvert t3)
 closureConvert (Print _ s t1) = return $ IrPrint s t1
-
+closureConvert (Let _ n ty tm (Sc1 t)) = do decl <- closureConvert tm
+                                            sc <- closureConvert t 
+                                            return $ IrLet n (convertType ty) decl sc
+closureConvert (App _ t1 t2) = --seria un ircall pero how y pq lista en 2do  
+-- app y fix ?
 
 convertType :: Ty -> IrTy
 convertType NatTy = IrInt
-convertType FunTy = IrClo
+convertType (FunTy _ _) = IrClo
 
 runCC' :: [Decl TTerm] -> M [IrDecl]
 runCC' [] = return []
